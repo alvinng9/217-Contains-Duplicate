d01499e442e63e62259bf532f5bca2743c8d0f66
Alvin <alvinngg9@gmail.com>
2024-07-02 16:12:21 -0700

    Initial Push

---
+++ src/test/java/org/example/SolutionTest.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void containsDuplicateTest() {
+        Solution solution = new Solution();
+        Assert.assertTrue(solution.containsDuplicate(new int[]{1, 2, 3, 1}));
+        Assert.assertFalse(solution.containsDuplicate(new int[]{1, 2, 3, 4}));
+        Assert.assertTrue(solution.containsDuplicate(new int[]{1,1,1,3,3,4,3,2,4,2}));
+
+    }
+
+}
